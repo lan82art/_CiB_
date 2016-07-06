70cccd41b4a7b3d6f97b1fb4836c58c78421595b
Artem <lanart@ex.ua>
2016-07-06 16:23:37 +0300

    Commit

---
+++ frontend/views/profile/passwordChange.php
@@ -0,0 +1,32 @@
+<?php
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\PasswordChangeForm*/
+
+$this->title = 'Изменить пароль';
+$this->params['breadcrumbs'][] = ['label' => 'Профиль', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-profile-password-change">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="user-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'currentPassword')->passwordInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'newPassword')->passwordInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'newPasswordRepeat')->passwordInput(['maxlength' => true]) ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
+
+</div>
